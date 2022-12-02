b219c58c3bdf66aac1d89eed8c8bb827066aee66
Ahmed Ould Ebnou <ahmed.rudloff@gmail.com>
2022-12-02 13:55:56 +0100

    add & delete Formateur + css little update

--- src/Controller/FormateurController.php
+++ src/Controller/FormateurController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Formateur;
+use App\Form\FormateurType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -20,6 +22,49 @@ class FormateurController extends AbstractController
             'formateurs' => $formateurs,
         ]);
     }
+    /**
+     * @Route("/formateur/add", name="add_formateur")
+     */    
+    public function add(ManagerRegistry $doctrine, Formateur $formateur = null, Request $request): Response
+    {
+        if(!$formateur){
+            $formateur = new Formateur();
+        }
+
+        $form = $this->createForm(FormateurType::class, $formateur);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+
+            $formateur = $form->getData();
+            $entityManager = $doctrine->getManager();
+            // prepare
+            $entityManager->persist($formateur);
+            // insert into (execute)
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_formateur');
+
+        }
+
+        // vue pour afficher le formulaire d'ajout
+        return $this->render('formateur/add.html.twig', [
+            'formAddFormateur' => $form->createView(),
+            'edit' => $formateur->getId()
+        ]);
+
+    }
+    /**
+     * @Route("/formateur/{id}/delete", name="delete_formateur")
+     */
+    public function delete(ManagerRegistry $doctrine, Formateur $formateur){
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($formateur);
+        $entityManager->flush();
+        return $this->redirectToRoute('app_formateur');
+    }
+    
      /**
      * @Route("/formateur/{id}", name="show_formateur")
      */
